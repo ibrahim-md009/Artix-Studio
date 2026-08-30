06b50dc6930c28662d7caa63b209986dc5fd13e9
ibrahim-md009 <ibrahim.shubair.dev@gmail.com>
2026-08-30 16:48:21 +0300

    update config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+// لو هتستخدم Authentication كمان
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -15,3 +16,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore and export
+export const db = getFirestore(app);
+
+// لو محتاج Auth
+export const auth = getAuth(app);
