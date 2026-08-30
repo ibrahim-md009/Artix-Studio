75703ceb2f6536a1df898093b44e4542bdd875c5
ibrahim-md009 <ibrahim.shubair.dev@gmail.com>
2026-08-30 16:46:07 +0300

    update firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,15 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "...",
+  apiKey: "AIzaSyC5XysGYa8_6Z_pZVCbwWKylWTqpSgSy44",
   authDomain: "photography-studio-ae81a.firebaseapp.com",
   projectId: "photography-studio-ae81a",
   storageBucket: "photography-studio-ae81a.firebasestorage.app",
   messagingSenderId: "1011841824778",
-  appId: "1:1011841824778:web:02d2bafec695a650cc07f3",
+  appId: "1:1011841824778:web:6d6e8fbb2971dc47cc07f3",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
